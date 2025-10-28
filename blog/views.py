35b7e6e842883d4397924730b734dd3274fb6afb
avin <avinineramz@gmail.com>
2025-10-28 00:56:49 -0700

    Dark_mode_added

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect
 from .models import Post
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+
+def post_new(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        Post.objects.create(title=title, content=content, created_at=timezone.now())
+    return redirect(request, 'blog/post_new.html')
 
 def home(request):
-    posts = Post.objects.all() # همه ی پست هارو از دیتابیس میگیره
+    posts = Post.objects.all().order_by('-created_at')
     return render(request, 'blog/home.html', {'posts': posts})
 
 def post_detail(request, pk):
